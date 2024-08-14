51b9187c459fd3c12115681bd5bc7941e71e63c9
gabrieulevi <gabriel@newnet.com.br>
2024-08-14 17:21:15 -0300

    adicionando limpa bloqueio como beta

--- frontend/tools-app/src/app/page.tsx
+++ frontend/tools-app/src/app/page.tsx
@@ -3,6 +3,13 @@ import { ModeToggle } from "@/components/ui/theme-toggle";
 import PageBackground from "@/components/project/pageBackgorund";
 import MenuCard from "@/components/project/menuCard";
 import { KeyRound, Calculator, PhoneMissed } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 export default function Home() {
   return (
@@ -10,42 +17,51 @@ export default function Home() {
       <div className="nav-bar flex flex-row items-center justify-between w-100 ">
         <Logo></Logo>
         <div className="right-container">
-        <ModeToggle />
+          <ModeToggle />
         </div>
       </div>
       <main className="flex h-full flex-col items-center justify-center p-24">
         <div className="main-menu w-4/6 h-3/6 grid grid-cols-1 gap-4 md:grid-cols-3">
-          <MenuCard 
-          link="/password-generator"
-          className="w-full transition-all  dark:hover:bg-zinc-50/5 backdrop-blur-md"
-          title="Gerador de senhas" 
-          description="Gerador de senhas simples"
-          content="Gere senhas com a complexidade ideal para a maioria dos sites e plataformas"
-          footer="v1.0"
+          <MenuCard
+            link="/password-generator"
+            className="w-full transition-all  dark:hover:bg-zinc-50/5 backdrop-blur-md"
+            title="Gerador de senhas"
+            description="Gerador de senhas simples"
+            content="Gere senhas com a complexidade ideal para a maioria dos sites e plataformas"
+            footer="v1.0"
           >
             <KeyRound />
           </MenuCard>
-          <MenuCard 
-          link="/instal-calc"
-          className="w-full transition-all  dark:hover:bg-zinc-50/5 backdrop-blur-md"
-          title="Calculadora de Instalação" 
-          description="Calculo de valor"
-          content="Calcule o valor de instalação específica para a situação com base no ultimo custo da fibra"
-          footer="v1.0"
+          <MenuCard
+            link="/instal-calc"
+            className="w-full transition-all  dark:hover:bg-zinc-50/5 backdrop-blur-md"
+            title="Calculadora de Instalação"
+            description="Calculo de valor"
+            content="Calcule o valor de instalação específica para a situação com base no ultimo custo da fibra"
+            footer="v1.0"
           >
             <Calculator />
           </MenuCard>
-          <MenuCard 
-          link="/"
-          className="w-full transition-all  dark:hover:bg-zinc-50/5 backdrop-blur-md"
-          title="Limpa bloqueio Simples IP" 
-          description="Liberação de acesso"
-          content="Veja lista de endereços bloqueados, pesquise presença dentro da lista de bloqueio e desbloqueie "
-          footer="v1.0"
+          <MenuCard
+            link="http://10.0.2.9:3000/limpabloqueio.html"
+            className="w-full transition-all  dark:hover:bg-zinc-50/5 backdrop-blur-md"
+            title="Limpa bloqueio Simples IP"
+            description="Liberação de acesso"
+            content="Veja lista de endereços bloqueados, pesquise presença dentro da lista de bloqueio e desbloqueie "
+            footer="v0.1"
           >
-            <PhoneMissed />
+            <div className="flex flex-row justify-between">
+              <PhoneMissed />
+              <HoverCard>
+                <HoverCardTrigger><Badge variant={"destructive"}>Beta</Badge></HoverCardTrigger>
+                <HoverCardContent className="dark:bg-zinc-900/60 bg-zinc-50/50 backdrop-blur-md">
+                  O sistema já roda em um outro servidor mas ainda não teve updates de frontend para se encaixar com o padrão atual
+                </HoverCardContent>
+              </HoverCard>
+
+              
+            </div>
           </MenuCard>
-         
         </div>
         <PageBackground text="Home" />
       </main>
