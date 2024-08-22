fd4ceed35cb11f7a4abb49ab108cf407ff8df0cb
gabrieulevi <gabriel@newnet.com.br>
2024-08-22 16:03:26 -0300

    changing env variables

--- frontend/tools-app/src/config/env.ts
+++ frontend/tools-app/src/config/env.ts
@@ -1,5 +1,5 @@
 export const API = {
     // CHECKLIST: "http://10.0.0.206:8000",
-    CHECKLIST: "http://10.0.2.9:48013",
-    TOOLS: "http://10.0.2.9:3000"
+    CHECKLIST: "https://187.103.0.132/checklist_api",
+    TOOLS: "https://187.103.0.132/tools_api"
 }
