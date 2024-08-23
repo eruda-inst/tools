b0217fd9e44971542e5dcafec80d365a34b26254
gabrieulevi <gabriel@newnet.com.br>
2024-08-23 09:04:05 -0300

    updating .env

--- frontend/tools-app/src/config/env.ts
+++ frontend/tools-app/src/config/env.ts
@@ -1,5 +1,5 @@
 export const API = {
     // CHECKLIST: "http://10.0.0.206:8000",
-    CHECKLIST: "https://187.103.0.132/checklist_api",
-    TOOLS: "https://187.103.0.132/tools_api"
+    CHECKLIST: "https://toolbox.newnet.com.br/checklist_api",
+    TOOLS: "https://toolbox.newnet.com.br/tools_api"
 }
